65e29947cea2ada7e56e5818c34a6993f4df2aed
varsha <varsha.vani12@gmail.com>
2020-06-30 14:27:10 -0700

    added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -20,9 +20,10 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that supports using email instead of username"""
