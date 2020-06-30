535e5e753164e8205cac012738a707a297ff488e
varsha <varsha.vani12@gmail.com>
2020-06-30 14:21:12 -0700

    added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -5,34 +5,32 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
 
-    def create_user(self,email,password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """creates and saves a new user"""
         if not email:
             raise ValueError('users must have an email address')
-        user =self.model(email=self.normalize_email(email),**extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self,email,password):
+    def create_superuser(self, email, password):
         """creates and saves a new superuser"""
-        user = self.create_user(email,password)
-        user.is_staff=True
-        user.is_superuser=True
-        user.save(using=self._db)
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
 
         return user
 
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that supports using email instead of username"""
-    email= models.EmailField(max_length=255,unique=True)
-    name=models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
-    objects= UserManager()
+    objects = UserManager()
 
-    USERNAME_FIELD ='email'
+    USERNAME_FIELD = 'email'
